76619e360f010be35f7f0bf982d097874a248083
yasir <yasir.m@shadbox.com>
2024-12-31 15:51:44 +0530

    fix navbar

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,8 +4,8 @@ class Message < ApplicationRecord
 	validates :body, presence: true
   	has_one_attached :media_file
 
-  	def thumbnail
-  		self.media_file.variant(resize: "300x300")
-  	end
+  	# def thumbnail
+  	# 	self.media_file.variant(resize: "300x300")
+  	# end
 
 end
